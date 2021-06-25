b5e2f052792c906a499a24022dc5f14e44e94afa
Marat T <W40k@mail.ru>
2021-06-25 20:00:06 +0300

    testHang

---
+++ tests/testHang.ino
@@ -0,0 +1,23 @@
+const uint8_t pinHANG = 4;
+
+bool isHangUp()
+{
+	return digitalRead(pinHANG);
+}
+
+void setup()
+{
+	pinMode(pinHANG,INPUT_PULLUP);
+	Serial.begin(9600);
+	delay(1000);
+}
+
+void loop()
+{
+	Serial.print("State:");
+	if (isHangUp())
+		Serial.println("Up");
+	else
+		Serial.println("Down");
+	delay(100);
+}
