28f3c05477ccebe1a1d9bb56d33c3a781922ecc7
Marat T <W40k@mail.ru>
2021-07-23 23:53:37 +0300

    test Bell

--- tests/testBell.ino
+++ tests/testBell.ino
@@ -1,15 +1,32 @@
-const unsigned int pinBELL	= 2;	// Bell
-const uint16_t BELL_FREQ = 20;	// Bell frequency
+const uint8_t pinBellForward  = 10;  // Bell left
+const uint8_t pinBellBack  = 11;  // Bell right
+const uint16_t BELL_FREQ = 75;  // Bell frequency
+
+void bellLeft();
+void bellRight();
 
 void setup()
 {
-	pinMode(pinBELL, OUTPUT);
+    pinMode(pinBellForward, OUTPUT);
+    pinMode(pinBellBack, OUTPUT);
 }
 
 void loop()
 {
-	digitalWrite(pinBELL, HIGH);
-	delay(BELL_FREQ);
-	digitalWrite(pinBELL, LOW);
-	delay(BELL_FREQ);
+    bellLeft();
+    delay(BELL_FREQ);
+    bellRight();
+    delay(BELL_FREQ);
+}
+
+void bellLeft()
+{
+    digitalWrite(pinBellForward, HIGH);
+    digitalWrite(pinBellBack, LOW);
+}
+
+void bellRight()
+{
+    digitalWrite(pinBellForward, LOW);
+    digitalWrite(pinBellBack, HIGH);
 }
